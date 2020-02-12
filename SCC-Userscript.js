33a8a1103cae4c92b84b82b0533e8b61fcc7de57
Mason Hunt <masonhunt130ii@gmail.com>
2020-02-13 05:05:34 +1100

    Changed the position of the @requires tag in the metadata block for literally no reason other than it looks better having the big set of @match tags at the very end of the metadata block.

--- SCC-Userscript.js
+++ SCC-Userscript.js
@@ -8,6 +8,7 @@
 // @homepage     https://github.com/Brittank88/Skip-CurseForge-Countdown
 // @downloadURL  https://raw.githubusercontent.com/Brittank88/Skip-CurseForge-Countdown/master/SCC-Userscript.js
 // @supportURL   https://github.com/Brittank88/Skip-CurseForge-Countdown/issues
+// @require      http://code.jquery.com/jquery-latest.js
 // @match        https://www.curseforge.com/minecraft/mc-mods/*
 // @match        https://www.curseforge.com/minecraft/mc-mods/*/files
 // @match        https://www.curseforge.com/minecraft/mc-mods/*/files/*
@@ -29,16 +30,15 @@
 // @match        https://www.curseforge.com/minecraft/worlds/*
 // @match        https://www.curseforge.com/minecraft/worlds/*/files
 // @match        https://www.curseforge.com/minecraft/worlds/*/files/*
-// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     // Get the reference to JQuery.
     var $ = window.jQuery;
 
     // For each a element containing a href that is clearly a download link for the file, append '/file' to the end (as this is the direct link).
-    $(`a[href^="${window.location.pathname.split('/files')[0]}/download"]`).each(function() {
+    $(`a[href^="${window.location.pathname.split('/files')[0]}/download"]`).each(function () {
         this.href = this.href.includes('?') ? this.href.split('?')[0] + '/file' : this.href + '/file';
     });
 })()
