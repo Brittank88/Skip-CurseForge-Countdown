762e3fb9de1fd5ee0d1de41bee3e0f5dbb70c77f
Mason Hunt <masonhunt130ii@gmail.com>
2020-02-13 00:44:51 +1100

    First release of the script!

---
+++ SCC-Userscript.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Skip CurseForge Countdown
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Skip the download countdown when downloading Minecraft mods from CurseForge.
+// @author       Brittank88
+// @copyright    2020+, Brittank88
+// @match        https://www.curseforge.com/minecraft/mc-mods/*
+// @match        https://www.curseforge.com/minecraft/mc-mods/*/files
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Get the reference to JQuery.
+    var $ = window.jQuery;
+
+    // For each a element containing a href that is clearly a download link for the file, append '/file' to the end (as this is the direct link).
+    $(`a[href^="${window.location.pathname.replace('/files', '')}/download"]`).each(function() {
+        this.href = this.href.includes('?') ? this.href.split('?')[0] + '/file' : this.href + '/file';
+    });
+})()
