57bc7b2aa4736e550af332cfdafdf517b68a3157
Mason Hunt <masonhunt130ii@gmail.com>
2020-02-27 22:22:45 +1100

    Working update that utilises wildcards and a better url-getting method to target 99% of download buttons for every game's available file on CurseForge..

--- SCC-Userscript.js
+++ SCC-Userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Skip CurseForge Countdown
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.4
 // @description  Skip the download countdown when downloading Minecraft mods from CurseForge.
 // @author       Brittank88
 // @copyright    2020+, Brittank88
@@ -9,27 +9,9 @@
 // @downloadURL  https://raw.githubusercontent.com/Brittank88/Skip-CurseForge-Countdown/master/SCC-Userscript.js
 // @supportURL   https://github.com/Brittank88/Skip-CurseForge-Countdown/issues
 // @require      http://code.jquery.com/jquery-latest.js
-// @match        https://www.curseforge.com/minecraft/mc-mods/*
-// @match        https://www.curseforge.com/minecraft/mc-mods/*/files
-// @match        https://www.curseforge.com/minecraft/mc-mods/*/files/*
-// @match        https://www.curseforge.com/minecraft/modpacks/*
-// @match        https://www.curseforge.com/minecraft/modpacks/*/files
-// @match        https://www.curseforge.com/minecraft/modpacks/*/files/*
-// @match        https://www.curseforge.com/minecraft/mc-addons/*
-// @match        https://www.curseforge.com/minecraft/mc-addons/*/files
-// @match        https://www.curseforge.com/minecraft/mc-addons/*/files/*
-// @match        https://www.curseforge.com/minecraft/customization/*
-// @match        https://www.curseforge.com/minecraft/customization/*/files
-// @match        https://www.curseforge.com/minecraft/customization/*/files/*
-// @match        https://www.curseforge.com/minecraft/bukkit-plugins/*
-// @match        https://www.curseforge.com/minecraft/bukkit-plugins/*/files
-// @match        https://www.curseforge.com/minecraft/bukkit-plugins/*/files/*
-// @match        https://www.curseforge.com/minecraft/texture-packs/*
-// @match        https://www.curseforge.com/minecraft/texture-packs/*/files
-// @match        https://www.curseforge.com/minecraft/texture-packs/*/files/*
-// @match        https://www.curseforge.com/minecraft/worlds/*
-// @match        https://www.curseforge.com/minecraft/worlds/*/files
-// @match        https://www.curseforge.com/minecraft/worlds/*/files/*
+// @match        https://www.curseforge.com/*/*/*
+// @match        https://www.curseforge.com/*/*/*/*
+// @match        https://www.curseforge.com/*/*/*/*/*
 // ==/UserScript==
 
 (function () {
@@ -38,7 +20,7 @@
     var $ = window.jQuery;
 
     // For each a element containing a href that is clearly a download link for the file, append '/file' to the end (as this is the direct link).
-    $(`a[href^="${window.location.pathname.split('/files')[0]}/download"]`).each(function () {
+    $(`a[href^="${$('li[id^="nav-description"] > a').first().attr('href')}/download"]`).each(function () {
         this.href = this.href.includes('?') ? this.href.split('?')[0] + '/file' : this.href + '/file';
     });
 })()
